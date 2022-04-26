2a9b4e4ce4012efba107513539417e15777115b9
Muhammad Azka <hello.mazka@gmail.com>
2022-04-26 19:22:41 +0700

    Added QR Reader feature

--- pages/addkey/index.jsx
+++ pages/addkey/index.jsx
@@ -1,59 +1,78 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { React } from "react";
+import { QrReader } from "react-qr-reader";
 import Layout from "../../components/layouts/baseLayout";
 import Alert from "../../components/reusables/alert";
-import AddkeyHandler from "./index.handler";
 import Container from "../../components/reusables/styles/container";
+import AddKeyHandler from "./index.handler";
 
 export default function Addkey() {
-  const handler = AddkeyHandler();
+  const handler = AddKeyHandler();
   return (
     <Layout title="Add Key">
       <Container>
-        <div className="flex justify-center w-full items-center">
-          <form className="w-full">
-            <div className="flex flex-col">
-              <input
-                className="border-2 border-slate-500 p-2 my-2"
-                placeholder="Label"
-                name="label"
-                type="text"
-                id="label"
-                onChange={(e) => handler.setLabelState(e.target.value)}
-                value={handler.labelState}
+        <div className="flex justify-center w-full items-center flex-col">
+          {handler.loadReader ? (
+            <>
+              <QrReader
+                onResult={(result, error) => {
+                  handler.handleReader(result, error);
+                }}
+                className="w-full"
+                constraints={{
+                  facingMode: "environment",
+                }}
               />
-              <input
-                className="border-2 border-slate-500 p-2 my-2"
-                placeholder="Issuer"
-                name="issuer"
-                type="text"
-                id="issuer"
-                onChange={(e) => handler.setIssuerState(e.target.value)}
-                value={handler.issuerState}
-              />
-              <input
-                className="border-2 border-slate-500 p-2 my-2"
-                placeholder="Secret"
-                name="secret"
-                type="text"
-                id="secret"
-                onChange={(e) => handler.setSecretState(e.target.value)}
-                value={handler.secretState}
-              />
-              <div className="flex flex-row">
-                <button className="bg-slate-600 hover:bg-slate-700 text-white font-bold py-4 px-4 my-4 w-1/2 mr-2">
-                  Scan QR
-                </button>
-                <button
-                  className="bg-green-600 hover:bg-green-700 text-white font-bold py-4 px-4 my-4 w-1/2 ml-2"
-                  onClick={handler.handleSubmit}
-                  type="button"
-                >
-                  Add Key
-                </button>
+            </>
+          ) : (
+            <form className="w-full">
+              <div className="flex flex-col">
+                <input
+                  className="border-2 border-slate-500 p-2 my-2"
+                  placeholder="Label"
+                  name="label"
+                  type="text"
+                  id="label"
+                  onChange={(e) => handler.setLabelState(e.target.value)}
+                  value={handler.labelState}
+                />
+                <input
+                  className="border-2 border-slate-500 p-2 my-2"
+                  placeholder="Issuer"
+                  name="issuer"
+                  type="text"
+                  id="issuer"
+                  onChange={(e) => handler.setIssuerState(e.target.value)}
+                  value={handler.issuerState}
+                />
+                <input
+                  className="border-2 border-slate-500 p-2 my-2"
+                  placeholder="Secret"
+                  name="secret"
+                  type="text"
+                  id="secret"
+                  onChange={(e) => handler.setSecretState(e.target.value)}
+                  value={handler.secretState}
+                />
+                <div className="flex flex-row">
+                  <button
+                    className="bg-slate-600 hover:bg-slate-700 text-white font-bold py-4 px-4 my-4 w-1/2 mr-2"
+                    onClick={() => handler.setLoadReader(true)}
+                    type="button"
+                  >
+                    Scan QR
+                  </button>
+                  <button
+                    className="bg-green-600 hover:bg-green-700 text-white font-bold py-4 px-4 my-4 w-1/2 ml-2"
+                    onClick={handler.handleSubmit}
+                    type="button"
+                  >
+                    Add Key
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          )}
           {handler.showSuccess && (
             <Alert style="success" text="Successfully adding key!" />
           )}
